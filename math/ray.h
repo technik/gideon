38fb15a2374a58a1446b875136c245f1114164a9
technik <cjtech90@gmail.com>
2018-04-26 20:03:56 +0200

    Ray.

---
+++ math/ray.h
@@ -0,0 +1,48 @@
+//-------------------------------------------------------------------------------------------------
+// Toy path tracer
+//-------------------------------------------------------------------------------------------------
+// Based on the minibook 'Raytracing in one weekend' and Aras P.'s series: Daily pathtracer
+// https://aras-p.info/blog/
+//--------------------------------------------------------------------------------------------------
+// Copyright 2018 Carmelo J Fdez-Aguera
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#pragma once
+
+#include "vector3.h"
+
+namespace math
+{
+	class Ray
+	{
+	public:
+		Ray() = default;
+		Ray( const Vec3f& ro ///< Ray origin
+			,const Vec3f& rd ///< Ray direction
+		)
+			: mOrigin(ro)
+			, mDirection(rd)
+		{};
+
+		const Vec3f& origin() const { return mOrigin; }
+		const Vec3f& direction() const { return mDirection; }
+		Vec3f at(float t) const { return mOrigin + t * mDirection; }
+
+	private:
+		Vec3f mOrigin;
+		Vec3f mDirection;
+	};
+}
